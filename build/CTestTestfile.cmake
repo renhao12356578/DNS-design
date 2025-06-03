194a0c04928df21d5da1ad2f89af75b3e7a36e9f
recover-DEV <pqy2004@foxmail.com>
2025-06-04 00:01:25 +0800

    up

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: R:/SCS NETWORK DESIGN/design demo
-# Build directory: R:/SCS NETWORK DESIGN/design demo/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
